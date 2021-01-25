b8e8baa5b586eb9455c0d592e3b75a9acaa1030c
farz1212 <farzaad1212@gmail.com>
2021-01-24 19:07:47 -0500

    Initial Commit

---
+++ MedalCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedalCase
+//
+//  Created by Farzaad Goiporia on 2021-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
